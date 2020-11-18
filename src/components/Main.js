e1b4825fa611ba22c7b483a2f4ee0933e2ba80ba
McKay Nelson <tmckaynelson@gmail.com>
2020-11-18 13:18:03 -0700

    working links for projects

--- src/components/Main.js
+++ src/components/Main.js
@@ -211,7 +211,7 @@ class Main extends React.Component {
           <p>
             React | Node.js | Express | Bcrypt | Material UI | HTML5 | CSS3 | Axios | PostgresSQL | Nodemailer
           </p>
-          <p>Live site: <a className="broken" href="http://167.71.147.170:8080/" target="_blank">167.71.147.170</a> (Currently down due to Digital Ocean account issues)</p>
+          <p>Live site: <a href="http://165.227.88.106:8081/" target="_blank">165.227.88.106</a></p>
           <p>Github repo: <a href="https://github.com/The-Ringers/Warehouse" target="_blank">github.com/The-Ringers/Warehouse</a></p>
 
           <hr/>
@@ -224,7 +224,7 @@ class Main extends React.Component {
           <p>
             React | Node.js | Express | Bcrypt | IGDB API | HTML5 | CSS3 | Axios | PostgresSQL
           </p>
-          <p>Live site: <a className="broken" href="http://167.71.254.138:8080/" target="_blank">167.71.254.138</a> (Currently down due to Digital Ocean account issues)</p>
+          <p>Live site: <a href="http://165.227.88.106:8080/" target="_blank">165.227.88.106</a></p>
           <p>Github repo: <a href="https://github.com/tmckaynelson/rgb-clan" target="_blank">github.com/tmckaynelson/rgb-clan</a></p>
           {close}
 
